f86ecf80b2a35c33ed4c7c8da61cba3225caa2d4
Ricardo Diaz <rdiaz@jacidi.com>
2022-12-14 22:22:45 -0400

    wip: asistencia

--- funciones.js
+++ funciones.js
@@ -5,7 +5,24 @@ class Registro {
     ced = 0;
     muni = "";
     edad = 0;
+
+    constructor(nombre, ced, muni, edad) {
+        this.nombre = nombre;
+        this.ced = ced;
+        this.muni = muni;
+        this.edad = edad;
+    }
+}
+
+class Participante {
+    nombre = "";
+    ced = 0;
+    muni = "";
+    edad = 0;
     calificado = true;
+    horaInicio;
+    horaFin;
+
 
     constructor(nombre, ced, muni, edad) {
         this.nombre = nombre;
@@ -17,13 +34,27 @@ class Registro {
 
 
 // Funciones para continuar a la sig pag
-function registro(){
+function redirectRegistro(){
     location.href = "registro.html";
 }
 
-function iniciarEvento(){
+function redirectResultados(){
     location.href = "resultados.html"
 }
+
+function redirectPrincipal(){
+    location.href = "index.html"
+}
+
+function redirectAsistencia(){
+    location.href = "asistencia.html"
+}
+
+// Variables Globales
+
+let registros = [];
+let participantes = [];
+let aux;
 
 // Funcion para que ningun parametro quede vacio
 
@@ -57,26 +88,82 @@ data.addEventListener("submit", function(eve){
         let dateform = new FormData(data);
 
         let tablaRef = document.getElementById("Tabla");
-        let filatablaRef = tablaRef.insertRow(-1);
+        let rowRef = tablaRef.insertRow(-1);
 
-        let añadirCellRef = filatablaRef.insertCell(0);
-        añadirCellRef.textContent = dateform.get("nombre")
-            
-        añadirCellRef = filatablaRef.insertCell(1);
-        añadirCellRef.textContent = dateform.get("cedula")
+        const name = dateform.get("nombre");
+        const ced = dateform.get("cedula");
+        const mun = dateform.get("municipio");
+        const age = dateform.get("edad");
 
-        añadirCellRef = filatablaRef.insertCell(2);
-        añadirCellRef.textContent = dateform.get("municipio")
 
-        añadirCellRef = filatablaRef.insertCell(3);
-        añadirCellRef.textContent = dateform.get("edad")
+        let cellRef = rowRef.insertCell(0);
+        cellRef.textContent = name;
 
+        cellRef = rowRef.insertCell(1);
+        cellRef.textContent = ced;
+
+        cellRef = rowRef.insertCell(2);
+        cellRef.textContent = mun;
+
+        cellRef = rowRef.insertCell(3);
+        cellRef.textContent = age;
+
+        registros.push(new Registro(name, ced, mun, age))
+        window.localStorage.setItem("registros", JSON.stringify(registros))
         data.reset()
     }
 })
 
+// Asistencia
 
-// Metodos Simulación Carrera
+function checkBoxChange(checkbox){
+    aux = checkbox;
+    if(checkbox.checked){
+        // Se activo el check, se guardara en participantes
+        const reg = registros.find(reg => reg.ced.toString() === checkbox.value);
+
+        participantes.push(new Participante(reg.nombre, reg.ced, reg.muni, reg.edad));
+    } else {
+        // Se desactivó el check, se elimina de participantes
+        const part = participantes.find(p => p.ced.toString() === checkbox.value);
+        participantes.splice(participantes.indexOf(part), 1);
+    }
+    // Se actualiza el item de participantes
+    localStorage.setItem("participantes", JSON.stringify(participantes));
+
+}
+
+function loadAsistenciaTable(){
+    // Se cargan los registros y se limpian los participantes
+    registros = JSON.parse(localStorage.getItem("registros"));
+    participantes = [];
+    localStorage.setItem("participantes", JSON.stringify(participantes));
+
+    let tablaRef = document.getElementById("tabla-asistencia");
+
+    // Se llena la tabla con los registros
+    registros.forEach(registro => {
+        let rowRef = tablaRef.insertRow(-1);
+
+        let cellRef = rowRef.insertCell(0);
+        cellRef.innerHTML = "<label>\n" +
+            "                            <input type=\"checkbox\" onchange=\"checkBoxChange(this)\" value=\"" +registro.ced + "\">\n" +
+            "                        </label>"
+        cellRef = rowRef.insertCell(1);
+        cellRef.textContent = registro.nombre;
+
+        cellRef = rowRef.insertCell(2);
+        cellRef.textContent = registro.ced;
+
+        cellRef = rowRef.insertCell(3);
+        cellRef.textContent = registro.muni;
+
+        cellRef = rowRef.insertCell(4);
+        cellRef.textContent = registro.edad;
+    })
+}
+
+
 // funcionamiento del campeonato
 
 function comienza(){
@@ -85,13 +172,6 @@ function comienza(){
     persona = (7 * 1000) / 3600, nadador = 1.72, ciclista = (45 * 1000) / 3600;
     
     candidatos = 0 // Se tiene que agregar la cantidad de candidatos que van a concursar
-let registros = [
-    new Registro("Par 1", 123456789, "Muni 1", 18),
-    new Registro("Par 2", 987654321, "Muni 2", 19),
-    new Registro("Par 3", 159753268, "Muni 3", 20),
-    new Registro("Par 4", 684215973, "Muni 4", 21)
-]
-
     distanPersonas = []
     disNadador = []
     disCiclista = []
@@ -141,5 +221,9 @@ function tercero(valorMaximoNadado){ // Ciclismo
     }
     valorMaximoPedaleando = Math.max(...disNadador)
     return valorMaximoPedaleando
+}
+
+function readResultados(){
+    registros = JSON.parse(localStorage.getItem("registros"));
 }
 
